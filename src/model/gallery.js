84d92b25832d79c863454882ed1c0720ead71e00
Narvalo17 <102968094+Narvalo17@users.noreply.github.com>
2025-10-22 15:48:38 +0200

    ajout d'image

---
+++ src/model/gallery.js
@@ -0,0 +1,104 @@
+const gallery = [
+  {
+    id: 1,
+    title: "chaton",
+    imgpath: "../assets/animal1.jpg",
+    description: "chaton mignon",
+    likeNumber:0 ,
+    category: "animals",
+  },
+    {
+        id: 2,
+        title: "sourire",
+        imgpath: "../assets/people2.jpg",
+        description: "souri a la vie et la vie te sourira",
+        likeNumber:0 ,
+        category: "people",
+    },
+    {
+        id: 3,
+        title: "montagne",
+        imgpath: "../assets/nature2.jpg",
+        description: "belle montagne",
+        likeNumber:0 ,
+        category: "nature",
+    },
+    {
+        id: 4,
+        title: "un ourse ",
+        imgpath: "../assets/animal2.jpg",
+        description: "un ourse qui dit salam alikouml",
+        likeNumber:0 ,
+        category: "animals",
+    },
+    {
+        id: 5,
+        title: "pont",
+        imgpath: "../assets/architecture4.jpg",
+        description: "un pont qui relie deux monde", 
+        likeNumber:0 ,
+        category: "architecture",
+    },
+    {
+        id: 6,
+        title: "Machin Learning",
+        imgpath: "../assets/techno2.jpg",
+        description: "le futur est la",
+        likeNumber:0 ,
+        category: "technology",
+    },
+     {
+        id: 7,
+        title: "coucher de soleil",
+        imgpath: "../assets/nature3.jpg",
+        description: "coucher de soleil sur la montagne",
+        likeNumber:0 ,
+        category: "nature",
+     },
+    {
+        id: 8,
+        title: "gratte ciel",
+        imgpath: "../assets/architecture2.jpg",
+        description: "un gratte ciel a dubai",
+        likeNumber:0 ,
+        category: "architecture",
+     },
+     {
+        id: 9,
+        title: "robot",
+        imgpath: "../assets/techno5.jpg",
+        description: "robot du futur",
+        likeNumber:0 ,
+        category: "technology",
+     },
+     {
+        id: 10,
+        title: "tigre",
+        imgpath: "../assets/animal3.jpg",
+        description: "un tigre qui court dans la prairie",
+        likeNumber:0 ,
+        category: "animals",
+     },
+     {
+        id: 11,
+        title: "homme  souriant",
+        imgpath: "../assets/people3.jpg",
+        description: "une femme qui sourit a la vie",
+        likeNumber:0 ,
+        category: "people",
+     },
+     {
+        
+     }
+    
+];
+
+
+const category = [
+  "nature",
+  "architecture",
+  "animals",
+  "technology",
+  "people",
+
+];
